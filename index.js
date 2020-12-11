39fd819b784c3d0e29c23031ce8f10cc16550333
Tucker <leachtucker@gmail.com>
2020-12-10 17:24:00 -0800

    dotEnv fix for heroku

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 /* IMPORTS */
-require('dotenv').config();
+if (process.env.NODE_ENV !== "production") {
+    require("dotenv").config();
+}
 const express = require('express');
 const cors = require('cors');
 
